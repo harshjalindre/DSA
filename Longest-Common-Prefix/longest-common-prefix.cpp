2715ba34dd9b2cc3b1fda247680c30691c620fde
harshjalindre <92078558+harshjalindre@users.noreply.github.com>
2026-01-01 00:10:28 +0530

    Longest Common Prefix

---
+++ Longest-Common-Prefix/longest-common-prefix.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+std::string longestCommonPrefix(std::vector<std::string>& strs) {
+    if (strs.empty()) return "";
+
+    std::string prefix = strs[0];
+
+    for (int i = 1; i < strs.size(); i++) {
+        while (strs[i].find(prefix) != 0) {
+            prefix = prefix.substr(0, prefix.size() - 1);
+            if (prefix.empty()) return "";
+        }
+    }
+
+    return prefix;
+}
+
+int main(){
+
+    std::vector<std::string> strs = {"flower","flow","flight"};
+
+    std::cout << longestCommonPrefix(strs) << std::endl;
+
+    return 0;
+}
