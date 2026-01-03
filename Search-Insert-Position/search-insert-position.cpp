b4d620c5d522856fa4e76fccea102a5feff30ec2
harshjalindre <92078558+harshjalindre@users.noreply.github.com>
2026-01-03 23:14:09 +0530

    Search insert position

---
+++ Search-Insert-Position/search-insert-position.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+int searchInsert(std::vector<int>& nums, int target) {
+    int left = 0;
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return left;
+}
+
+int main() {
+    std::vector<int> nums = {1, 3, 5, 6};
+    int target = 5;
+
+    int vals = searchInsert(nums, target);
+
+    // Fixed: added semicolon and text for readability
+    std::cout << "Index: " << vals << std::endl;
+
+    std::cout << "Vector: ";
+    for (int i = 0; i < nums.size(); i++) {
+        std::cout << nums[i] << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
